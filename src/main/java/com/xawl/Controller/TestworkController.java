633889fa02095e3d67786d74c31b9588018226b0
Kiven <1132232330@qq.com>
2018-03-19 12:30:28 +0800

    将Testwork中的更新逻辑进行了改正；

--- src/main/java/com/xawl/Controller/TestworkController.java
+++ src/main/java/com/xawl/Controller/TestworkController.java
@@ -7,6 +7,7 @@ import com.xawl.Vo.ResultData;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -36,7 +37,7 @@ public class TestworkController {
         testwork.setPass(0);
         testwork.setStatedDate(new Timestamp(new Date().getTime()));
         List<Testwork> testworkList = testworkService.getTestwork(testwork1);//查询此用户之前是否插入过考试工作量的数据,在同一学期
-        if(testworkList!=null&&testworkList.size()>0&&testworkList.get(0).getPass()!=4) {
+        if (testworkList != null && testworkList.size() > 0 && testworkList.get(0).getPass() != 4) {
             if (testworkList.get(0).getJnum() != null && testworkList.get(0).getJnum() > 0)
                 return new ResultData(24, "existed");
             else {//如果已存在课程名插入就得更新总学时了
@@ -46,10 +47,10 @@ public class TestworkController {
                 return new ResultData(1);
             }
         }
-        System.out.println("uid:"+testwork.getUid());
-        System.out.println("getJnum:"+testwork.getJnum());
-        System.out.println("getJpclass:"+testwork.getJpclass());
-        System.out.println("getPclassNum:"+testwork.getPclassNum());
+        System.out.println("uid:" + testwork.getUid());
+        System.out.println("getJnum:" + testwork.getJnum());
+        System.out.println("getJpclass:" + testwork.getJpclass());
+        System.out.println("getPclassNum:" + testwork.getPclassNum());
         testworkService.insertTestwork(testwork);
         return new ResultData(1);
 
@@ -67,89 +68,122 @@ public class TestworkController {
         System.out.println("uid:" + testwork.getUid());
         testwork.setStatedDate(new Timestamp(new Date().getTime()));
         //计算标准课时
-        if (testwork.getMpclass() != null&&testwork.getMpclass()>0) {
-            testwork.setMpclass(Coe.testPaper);
-        }
-        if (testwork.getQpclass() != null&&testwork.getQpclass()>0) {
-            testwork.setQpclass(Coe.testPaper);
-        }
-        System.out.println("testWork.getMpclass:" + testwork.getMpclass());
-        System.out.println("testwork.getQpclass():" + testwork.getQpclass());
-        testwork.setPaperSum(testwork.getBpaperNum() + testwork.getCpaperNum() + testwork.getQpaperNum() + testwork.getPaperNum());
-        System.out.println("阅卷总份数:" + testwork.getPaperSum());
-        testwork.setPaperPclass(testwork.getPaperNum() * Coe.inspectTest);
-        testwork.setPclassNum(testwork.getPaperSum() + testwork.getQpclass() + testwork.getPaperPclass() + testwork.getJpclass());
-        System.out.println("总课时：" + testwork.getPclassNum());
-        Testwork testwork1 = new Testwork();
-        testwork1.setUid(testwork.getUid());
-        testwork1.setTerm(testwork.getTerm());
-        List<Testwork> testworksList = testworkService.getTestwork(testwork1);
-        if (testworksList != null && testworksList.size() > 0&&testwork.getPass()!=4) {
-            System.out.println("0000:" + testworksList);
-            if (testwork.getLname() == testworksList.get(0).getLname())
-                return new ResultData(24, "existed");
-            else {
-                testwork.setPclassNum(testwork.getPclassNum() + testworksList.get(1).getJpclass());
-                testwork.setId(testworksList.get(1).getId());
-                testworkService.updateTestworkById(testwork);
-                return new ResultData(1);
-            }
-
-        }
-        testworkService.insertTestwork(testwork);
-        return new ResultData(1);
-    }
-
-     /*
-     计算标准课时
-     * */
-
-  /*  Double calculateClasshours(Integer type, Integer num) {
-        Double Classhours = 0.0;
-        if (type == 1 || type == 2)
-            Classhours = (double) num * Coe.testPaper;
-        if (type == 3)
-            Classhours = (double) num * Coe.invigilate;
-        if (type == 4 || type == 5 || type == 6 || type == 7)
-            Classhours = (double) num * Coe.inspectTest;
-        return Classhours;
-    }*/
-
-    @RequestMapping("/user/updateTestworkById.action")
-    @ResponseBody
-    ResultData updateTestworkById(Testwork testwork) {
-        if (testwork == null && testwork.getId() == null)
-            return new ResultData(23);
-        if (testwork.getJnum() != null && testwork.getJnum() > 0) {
-            testwork.setJpclass(testwork.getJnum() * Coe.invigilate);
-        }
-        if ((testwork.getQpaperNum() != null && testwork.getQpaperNum() > 0) || (testwork.getCpaperNum() != null && testwork.getCpaperNum() > 0)
-                || (testwork.getBpaperNum() != null && testwork.getBpaperNum() > 0) || (testwork.getPaperNum() != null && testwork.getPaperNum() > 0)) {
-            testwork.setPaperSum(testwork.getBpaperNum() + testwork.getCpaperNum() + testwork.getQpaperNum() + testwork.getPaperNum());
-            testwork.setPaperPclass(testwork.getPaperSum() * Coe.inspectTest);
-        }
         if (testwork.getMpclass() != null && testwork.getMpclass() > 0) {
             testwork.setMpclass(Coe.testPaper);
         }
         if (testwork.getQpclass() != null && testwork.getQpclass() > 0) {
             testwork.setQpclass(Coe.testPaper);
         }
-        testwork.setPclassNum(testwork.getPaperSum() + testwork.getQpclass() + testwork.getPaperPclass() + testwork.getJpclass());
+        System.out.println("testWork.getMpclass:" + testwork.getMpclass());
+        System.out.println("testwork.getQpclass():" + testwork.getQpclass());
+        testwork.setPaperSum(testwork.getBpaperNum() + testwork.getCpaperNum() + testwork.getQpaperNum() + testwork.getPaperNum());
+        testwork.setPaperPclass(testwork.getPaperSum() * Coe.inspectTest);
+        testwork.setPclassNum(testwork.getMpclass() + testwork.getQpclass() + testwork.getPaperPclass());
+
+        Testwork testwork1 = new Testwork();
+        testwork1.setUid(testwork.getUid());
+        testwork1.setTerm(testwork.getTerm());
+        List<Testwork> testworksList = testworkService.getTestwork(testwork1);
+        if (testworksList != null && testworksList.size() > 0) {//详情见数据库设计中的附1
+            System.out.println("0000:" + testworksList);
+            if (testworksList.get(0).getLname() == null || testworksList.get(0).getLname() == "") {
+                testwork.setPclassNum(testwork.getPclassNum() + testworksList.get(0).getJpclass());
+                testwork.setId(testworksList.get(0).getId());
+                testworkService.updateTestworkById(testwork);
+                return new ResultData(1);
+            } else {
+                System.out.println("1.5 1.5 1.5");
+                System.out.println("testwork.getLname():" + testwork.getLname());
+                System.out.println("testworksList.get(0).getPass():" + testworksList.get(0).getPass());
+                if (testwork.getLname() == testworksList.get(0).getLname() || testwork.getLname().equals(testworksList.get(0).getLname())) {
+                    System.out.println("1111");
+                    if (testworksList.get(0).getPass() != 4) {
+                        System.out.println("222");
+                        return new ResultData(24, "existed");
+                    }
+                    System.out.println("333");
+                }
+            }
+        }
+        System.out.println("阅卷总份数:" + testwork.getPaperSum());
+        System.out.println("阅卷总课时:" + testwork.getPaperPclass());
         System.out.println("总课时：" + testwork.getPclassNum());
-        testworkService.updateTestworkById(testwork);
+        testworkService.insertTestwork(testwork);
         return new ResultData(1);
+    }
+
+
+    @RequestMapping("/user/updateTestworkById.action")
+    @ResponseBody
+    ResultData updateTestworkById(Testwork testwork) {
+        if (testwork == null || testwork.getId() == null)
+            return new ResultData(23);
+        Double pclassNum = 0.0;
+        Integer paperSum = 0;
+    /*    if ((testwork.getJnum() != null && testwork.getJnum() > 0)
+                || (testwork.getQpaperNum() != null && testwork.getQpaperNum() > 0)
+                || (testwork.getCpaperNum() != null && testwork.getCpaperNum() > 0)
+                || (testwork.getBpaperNum() != null && testwork.getBpaperNum() > 0)
+                || (testwork.getPaperNum() != null && testwork.getPaperNum() > 0)
+                || (testwork.getMpclass() != null && testwork.getMpclass() > 0)
+                || (testwork.getQpclass() != null && testwork.getQpclass() > 0))*/
+        //不加这句判断语句，只改名字总课时汇总时会报空指针异常
+        if (testwork.getJnum() != null && testwork.getJnum() >= 0) {
+            testwork.setJpclass(testwork.getJnum() * Coe.invigilate);
+            pclassNum += testwork.getJpclass();
+            System.out.println("pclassNum：" + pclassNum);
+        }
+        if (testwork.getMpclass() != null && testwork.getMpclass() > 0) {
+            testwork.setMpclass(Coe.testPaper);
+            pclassNum += testwork.getMpclass();
+            System.out.println("pclassNum：" + pclassNum);
+
+        }
+        if (testwork.getQpclass() != null && testwork.getQpclass() > 0) {
+            testwork.setQpclass(Coe.testPaper);
+            pclassNum += testwork.getQpclass();
+            System.out.println("pclassNum：" + pclassNum);
+
+        }
+        if (testwork.getQpaperNum() != null && testwork.getQpaperNum() > 0) {
+            paperSum += testwork.getQpaperNum();
+        }
+        if (testwork.getCpaperNum() != null && testwork.getCpaperNum() > 0) {
+            paperSum += testwork.getCpaperNum();
+        }
+        if (testwork.getBpaperNum() != null && testwork.getBpaperNum() > 0) {
+            paperSum += testwork.getBpaperNum();
+        }
+        if (testwork.getPaperNum() != null && testwork.getPaperNum() > 0) {
+            paperSum += testwork.getPaperNum();
+        }
+        if (testwork.getJnum() != null && testwork.getJnum() > 0){//因为传的数必须都传，所以任取一个即可
+            testwork.setPaperSum(paperSum);
+            testwork.setPaperPclass(testwork.getPaperSum() * Coe.inspectTest);
+            testwork.setPclassNum(pclassNum + testwork.getPaperPclass());
+            System.out.println("总阅卷数：" + testwork.getPaperSum());
+            System.out.println("总阅卷课时：" + testwork.getPaperPclass());
+            System.out.println("总课时：" + testwork.getPclassNum());
+        }
+
+
+        testworkService.updateTestworkById(testwork);
+        return new
+
+                ResultData(1);
+
     }
 
     @RequestMapping("/user/getTestwork.action")
     @ResponseBody
-    ResultData getTestwork(Testwork testwork,HttpSession session) {
+    ResultData getTestwork(Testwork testwork, HttpSession session) {
         if (testwork == null)
             testwork.setUid((Integer) session.getAttribute("uid"));
 
         return new ResultData(1, testworkService.getTestwork(testwork));
     }
 
-    @RequestMapping("/admin/deleteTestwork.action")
+    @RequestMapping("/admin/deleteTestworkById.action")
     @ResponseBody
     ResultData delectTestwork(Integer id) {
         if (id == null || id <= 0) {
